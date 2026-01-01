8237f79ab6acc448eb7aab179dccb3f4b65eea2b
KhaledZaabat <khaled.zaabat@ensia.edu.dz>
2026-01-01 12:50:20 +0100

    FEAT : add get groups

---
+++ backend/quiz-management-system/quiz-management-system.App/Controllers/GroupsController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using quiz_management_system.App.Helpers;
+using quiz_management_system.Application.Features.Groups.Queries.GetGroups;
+using quiz_management_system.Contracts.Reponses.Groups;
+
+namespace quiz_management_system.App.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[ApiVersion("1.0")]
+
+public class GroupsController(IMediator _mediator) : ControllerBase
+{
+    /// <summary>
+    /// Get all groups with their assigned academic years.
+    /// </summary>
+    /// <remarks>
+    /// Returns a list of all groups enriched with their academic year information.
+    /// This endpoint is cached for 24 hours to improve performance.
+    /// </remarks>
+    /// <returns>List of <see cref="GroupWithAcademicYearResponse"/> objects</returns>
+    /// <response code="200">Returns the list of groups</response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IReadOnlyList<GroupWithAcademicYearResponse>>> GetGroups(CancellationToken ct)
+    {
+        var query = new GetAllGroupsQuery();
+        var result = await _mediator.Send(query, ct);
+
+        return result.ToActionResult(HttpContext);
+    }
+}
---
+++ backend/quiz-management-system/quiz-management-system.Application/Features/Groups/Queries/GetGroups/GetAllGroupsHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using quiz_management_system.Contracts.Reponses.Groups;
+using quiz_management_system.Domain.Common.ResultPattern.Result;
+
+namespace quiz_management_system.Application.Features.Groups.Queries.GetGroups;
+
+public sealed class GetAllGroupsHandler(IAppDbContext _context)
+    : IRequestHandler<GetAllGroupsQuery, Result<IReadOnlyList<GroupWithAcademicYearResponse>>>
+{
+
+
+    public async Task<Result<IReadOnlyList<GroupWithAcademicYearResponse>>> Handle(
+        GetAllGroupsQuery request,
+        CancellationToken ct)
+    {
+        var groups = await _context.Groups
+            .AsNoTracking()
+            .Include(g => g.AcademicYear)
+            .Select(g => new GroupWithAcademicYearResponse(
+                g.Id,
+                g.GroupNumber,
+                g.AcademicYearId,
+                g.AcademicYear.Number
+            ))
+            .ToListAsync(ct);
+
+        return Result.Success<IReadOnlyList<GroupWithAcademicYearResponse>>(groups);
+    }
+}
